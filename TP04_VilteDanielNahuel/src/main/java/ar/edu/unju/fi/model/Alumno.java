d71204ff0edc8f5e5f53f9a631ae61ec1136cb26
Nahuel <Nahuel@DESKTOP-NAHUEL>
2024-05-25 23:47:13 -0300

    TP 4 Completado

---
+++ TP04_VilteDanielNahuel/src/main/java/ar/edu/unju/fi/model/Alumno.java
@@ -0,0 +1,17 @@
+package ar.edu.unju.fi.model;
+
+import java.time.LocalDate;
+
+public class Alumno {
+	private String dni;
+	private String nombre;
+	private String apell;
+	private String email;
+	private String telefono;
+	private LocalDate fecha_nac;
+	private String domicilio;
+	private String lu;
+	
+	
+	
+}
---
+++ TP04_VilteDanielNahuel/src/main/java/ar/edu/unju/fi/model/Materia.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.model;
+
+import ar.edu.unju.fi.model.*;
+
+public class Materia {
+	private String codigo;
+	private String nombre;
+	private String Curso;
+	private int cant_horas;
+	private String modalidad;
+	private Docente docente;
+	private Carrera carrera;
+}
